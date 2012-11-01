c2c4df713150595686b5a97cea1af9bd0e9402d7
Dan <dschaffer2003@yahoo.com>
2012-11-01 04:00:36 +0000

    Got some tests to run

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,18 +3,23 @@ require 'spec_helper'
 describe MoviesController do
   describe 'finding movies by the same director' do
     before :each do
+      @movie1 = mock('Movie')
+      @movie1.stub(:id).and_return('1')
+      @movie1.stub(:director).and_return('Ridley Scott')
       @fake_results = [mock('movie1'), mock('movie2')]
     end
     it 'should call the model method that searches for movies by the same director' do
+      Movie.stub(:find).and_return(@movie1)
       Movie.should_receive(:find_similar).with('Ridley Scott').
         and_return(@fake_results)
-      post :similar, {:director => 'Ridley Scott'}
+      post :similar, {:id => '1'}
     end
 
     describe 'after valid search' do
       before :each do
+        Movie.stub(:find).and_return(@movie1)
         Movie.stub(:find_similar).and_return(@fake_results)
-        post :similar, {:director => 'Ridley Scott'}
+        post :similar, {:id => '1'}
       end
       it 'should select the Same Director template for rendering' do
         response.should render_template('similar')
@@ -24,5 +29,6 @@ describe MoviesController do
         assigns(:movies).should == @fake_results
       end
     end
+
   end
 end
